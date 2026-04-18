433dc9094de0e2b3f915600ef7a1867f3bbe70ac
Michael Wells <wow@duhmarket.com>
2026-04-17 21:59:37 -0400

    Initial Commit

---
+++ ClipNet/ClipNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipNetApp.swift
+//  ClipNet
+//
+//  Created by Scott Leonard on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
